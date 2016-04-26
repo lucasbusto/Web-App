3db738d74b3bfffd90473153659081a9a7d42231
lucasbusto <lucasvvasconcelos@hotmail.com>
2016-04-26 02:23:35 -0300

    Update
    Changing the javascript.

--- js/block/card.js
+++ js/block/card.js
@@ -1,19 +1,35 @@
-var $cards = document.querySelectorAll('.card');
-var $cardColors = document.querySelectorAll('.card-colors');
+var $wrapCard = document.querySelector('.wrap-card');
+var $cardColors = document.querySelectorAll('.card-options');
 
-for (var index = 0; index < $cards.length; index++) {
-  $cards[index].addEventListener('click', function (event) {
-    var $this = event.target;
-    var $card = this;
 
-    if ($this.classList.contains('card-colors')) {
-      $card.dataset.color = $this.dataset.color;
+    $wrapCard.addEventListener('click', function(event) {
+        var $this = event.target;
+        var $card = $this.parentNode.parentNode.parentNode;
+        var $cardContent = $card.querySelector('.card-content');
 
-      for (var position = 0; position < $cardColors.length; position++) {
-        $cardColors[position].classList.remove('isActive');
-      };
+        if ($this.dataset.color) {
+            $card.dataset.color = $this.dataset.color;
 
-      $this.classList.add('isActive');
-    };
-  });
-};
+            for (var position = 0; position < $cardColors.length; position++) {
+                $cardColors[position].classList.remove('isActive');
+            };
+
+            $this.classList.add('isActive');
+        };
+
+        if ($this.classList.contains('card_delete')) {
+            $card.remove();
+        };
+
+        if ($this.classList.contains('card_edit')) {
+            if ($cardContent.getAttribute('contenteditable') == 'false') {
+                $cardContent.setAttribute('contenteditable', 'true');
+                $cardContent.focus();
+                $this.classList.add('isActive');
+            } else {
+                $cardContent.setAttribute('contenteditable', 'false');
+                $cardContent.blur();
+                $this.classList.remove('isActive');
+            };
+        };
+    });
